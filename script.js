4f6d7c9c47e9530d2f5b48159093235a2a3289d1
Andrew Chung <aandrewchung@gmail.com>
2023-07-29 23:31:21 +0900

    updated script.js file to include fns from contractbytecode.js

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
-// Initialize Web3 with your Ethereum node URL
-const { Web3 } = require('web3');
-const providerUrl = 'https://mainnet.infura.io/v3/1c9ccac844a046aba5d3e142f29bf976';
-const web3 = new Web3(providerUrl);
-
-// Function to read contract bytecode
-async function readContractBytecode(contractAddress) {
-  try {
-    // Get the contract's code from the blockchain
-    const bytecode = await web3.eth.getCode(contractAddress);
-    const first10Chars = bytecode.substring(0, 10); //only printing 10 characters for now
-
-    // Log the bytecode
-    console.log('Contract Bytecode:', first10Chars);
-  } catch (error) {
-    console.error('Error reading contract bytecode:', error);
-  }
-}
+// Import the functions from contractbytecode.js
+const { readContractBytecode, findAddressInBytecode } = require('./contractbytecode.js');
 
 // Contract address to fetch bytecode from
-const contractAddress = '0xBB9bc244D798123fDe783fCc1C72d3Bb8C189413';
+const contractAddress = "0x05770332D4410b6D7f07Fd497E4c00F8F7bFb74A";
+const referencedAddress = "0x8D6CeBD76f18E1558D4DB88138e2DeFB3909fAD6";
 
-// Call the function to read the contract bytecode
-readContractBytecode(contractAddress);
+(async () => {
+  // Use the functions from contractbytecode.js
+  const contractBytecode = await readContractBytecode(contractAddress);
+  // console.log(contractBytecode);
+  findAddressInBytecode(referencedAddress, contractBytecode);
+})();
