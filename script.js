11567ed450d547b4ca328ab9863832f7a45a1f08
Andrew Chung <aandrewchung@gmail.com>
2023-08-12 03:15:25 -0700

    updated script to check when a new contract is created

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-// Import the functions from contractbytecode.js
-const { readContractBytecode } = require('./contractbytecode.js');
+const { continuouslyGetContracts, eventEmitter } = require('./latestblock'); 
 
-// Contract address to fetch bytecode from
-const contractAddress = "0x05770332D4410b6D7f07Fd497E4c00F8F7bFb74A";
-const referencedAddress = "0x8D6CeBD76f18E1558D4DB88138e2DeFB3909fAD6";
+let eventCounter = 0;
 
-(async () => {
-  // Use the functions from contractbytecode.js
-  const contractBytecode = await readContractBytecode(contractAddress);
-  // console.log(contractBytecode);
-  findAddressInBytecode(referencedAddress, contractBytecode);
-})();
+// Attach an event listener for the 'newContracts' event
+eventEmitter.on('newContracts', (newContracts) => {
+  // Call your additional functions here, passing the newContracts as needed
+  // For example:
+  // myAdditionalFunction(newContracts);
+
+  
+
+  eventCounter++;
+  console.log(`Event emitted ${eventCounter} times. New contracts:`, newContracts);
+});
+
+// Call the continuouslyGetContracts() function
+continuouslyGetContracts();
