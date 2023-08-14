b7f272161a17929dac311c21b42b9a0a2d41226d
Andrew Chung <aandrewchung@gmail.com>
2023-08-14 14:55:53 -0700

    updated console log message

--- compareuserchain.js
+++ compareuserchain.js
@@ -1,10 +1,18 @@
 const fs = require('fs');
 const { isAddress } = require('web3-validator');
 
+// Initialize Web3 with your Ethereum node URL
+require('dotenv').config(); // Load environment variables from .env file
+const { Web3 } = require('web3');
+
+// Initialize Web3 with your Ethereum node URL
+const providerUrl = process.env.CHAIN1_PROVIDER_URL;
+const web3 = new Web3(providerUrl);
+
 // Import functions from other files
 const {loadUserDatabase, loadChainDatabase, loadUserChainDatabase, saveToUserChainDatabase} = require('./databasefns.js');
 const { readContractBytecode } = require('./contractbytecode.js');
-const { getUniqueAddressesForChainA, getUniqueAddressesForChainB } = require('./useraddress.js');
+const { getUniqueAddressesForChainA, getUniqueAddressesForChainB } = require('./useraddress.js');   
 
 async function findAddressInBytecode(inputAddress, contractAddress) {
     try {
@@ -36,17 +44,66 @@ async function findAddressInBytecode(inputAddress, contractAddress) {
     }
   }
 
+function getTransactionHash(chainIndex, blockNumber, contractAddress) {
+    const databasePath = `databases/chain${chainIndex}_database.json`;
+
+    if (fs.existsSync(databasePath)) {
+        const data = fs.readFileSync(databasePath, 'utf8');
+        const database = JSON.parse(data);
+        if (database[blockNumber] && database[blockNumber][contractAddress]) {
+        return database[blockNumber][contractAddress];
+        }
+    }
+
+    return null; // Transaction hash not found
+}
+
+async function printConsoleLog(chainIndex, blockNumber, contractAddress, inputAddress) {
+    try {
+        const transactionHash = getTransactionHash(chainIndex, blockNumber, contractAddress);
+        const block = await web3.eth.getBlock(blockNumber);
+
+        if (!block) {
+            console.log("Block not found.");
+            return;
+        }
+
+        const creationTime = new Date(Number(block.timestamp) * 1000); // Convert Unix timestamp to human-readable date
+        const etherscanBaseUrl = "https://etherscan.io";
+
+        const logMessage = `
+            Contract Address: ${contractAddress}
+            Input Address: ${inputAddress}
+            Chain Index: ${chainIndex}
+            Block Number: ${blockNumber}
+            Transaction Hash: ${transactionHash}
+            Creation Time: ${creationTime.toISOString()} (${creationTime.toLocaleString()})
+            Transaction Link: ${etherscanBaseUrl}/tx/${transactionHash}
+        `;
+
+        console.log(logMessage);
+    } catch (error) {
+        console.error("Error fetching block details:", error);
+    }
+}
+
+// // Example usage
+// const chainIndex = 1;
+// const blockNumber = "17442128";
+// const contractAddress = "0x05770332d4410b6d7f07fd497e4c00f8f7bfb74a";
+// const inputAddress = "0x8D6CeBD76f18E1558D4DB88138e2DeFB3909fAD6";
+// printConsoleLog(chainIndex, blockNumber, contractAddress, inputAddress);
 
 //Function to loop thru each newly created contracted for the block
 async function compareUserWithChain(chainIndex, blockNumber, blockAddresses) {
     const userData = loadUserDatabase();
     const userAddresses = getUniqueAddressesForChainB(userData, chainIndex); // Get unique user addresses based on chain number
 
-    for (const blockAddress of blockAddresses) { //loop thru newly created contract addy's
-        for (const userAddress of userAddresses) { //loop thru the user addresses
-            if (await findAddressInBytecode(userAddress, blockAddress)) {
-                console.log("SAVING TO DATABASE");
-                saveToUserChainDatabase(chainIndex, blockNumber, blockAddress, userAddress); //save to database            
+    for (const contractAddress of blockAddresses) { //loop thru newly created contract addy's
+        for (const inputAddress of userAddresses) { //loop thru the user addresses
+            if (await findAddressInBytecode(inputAddress, contractAddress)) {
+                printConsoleLog(chainIndex, blockNumber, contractAddress, inputAddress);
+                saveToUserChainDatabase(chainIndex, blockNumber, contractAddress, inputAddress); //save to database            
             }
         }
     }
@@ -111,4 +168,4 @@ function test() {
     
 }
 
-test();
+// test();
