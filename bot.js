1452f49016051904dffecf0503ef1ba69749fdee
Andrew Chung <aandrewchung@gmail.com>
2023-08-25 18:45:09 -0700

    bot messages users when cross check comes back tru

--- bot.js
+++ bot.js
@@ -1,63 +1,99 @@
-const TelegramBot = require('node-telegram-bot-api');
 const { saveToUser } = require('./updatedatabase');
+const { continuouslyGetContracts, eventEmitter } = require('./latestblock'); 
+const { compareUserWithChain, logEmitter } = require('./compareuserchain'); 
 
+const fs = require('fs');
+
+const TelegramBot = require('node-telegram-bot-api');
 const token = '6206698407:AAEopZeqzniqiNQJmu-fQmjM7gMKTYVWos8';
 const bot = new TelegramBot(token, { polling: true });
 
-// Command: /echo [whatever]
-bot.onText(/\/echo (.+)/, (msg, match) => {
-    const chatId = msg.chat.id;
-    const resp = match[1];
-    bot.sendMessage(chatId, resp);
+let eventCounter = 0;
+
+
+// Attach an event listener for the 'newContracts' event
+eventEmitter.on('newContracts', ({ contracts, chainIndex, blockNumber }) => {
+  eventCounter++;
+  console.log(`Event emitted ${eventCounter} times. New Contracts on chain ${chainIndex + 1}:`, contracts);
+  
+
+  // Call function to compare user addresses and chains
+  compareUserWithChain(chainIndex+1, blockNumber, contracts);
 });
 
-// Command: /add [userid]
-bot.onText(/\/add (\d+)/, (msg, match) => {
+// Attach an event listener for the 'logMessage' event
+logEmitter.on('logMessage', (logMessage) => {
+    // Read the user database from the file
+    let userDatabase = require('./databases/user_database_test.json');
+    
+    // Extract all user IDs (which are keys in the user database)
+    const userIDs = Object.keys(userDatabase);
+    
+    // Iterate through user IDs and send the log message to each user
+    userIDs.forEach(userID => {
+        bot.sendMessage(userID, logMessage);
+    });
+});
+
+// Command: /start
+bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    const userId = match[1];
-    bot.sendMessage(chatId, `Adding user with ID ${userId}`);
+    
+    // Read the user database from the file
+    let userDatabase = require('./databases/user_database_test.json');
+    
+    // Add the chat ID as a user ID
+    userDatabase[chatId.toString()] = {};
+    
+    // Write the updated user database back to the file
+    fs.writeFileSync('./databases/user_database_test.json', JSON.stringify(userDatabase, null, 2));
+    
+    bot.sendMessage(chatId, 'Welcome! Your chat ID has been stored.');
+});
+
+// Command: /echo [whatever]
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1];
+  bot.sendMessage(chatId, resp);
 });
 
 // Command: /addaddress [chain_name] [address1] [address2] ...
 bot.onText(/\/addaddress (\S+) (.+)/, (msg, match) => {
-    const chatId = msg.chat.id;
-    const chainName = match[1].toLowerCase(); // Convert chain name to lowercase
-    const addresses = match[2].split(' ');
+  const chatId = msg.chat.id;
+  const chainName = match[1].toLowerCase(); // Convert chain name to lowercase
+  const addresses = match[2].split(' ');
 
-    // Map chain names to their corresponding IDs
-    const chainMappings = {
-        ethereum: 1,
-        bsc: 2,
-        polygon: 3
-        // Add more chain names as needed
-    };
+  // Map chain names to their corresponding IDs
+  const chainMappings = {
+    ethereum: 1,
+    bsc: 2,
+    polygon: 3
+    // Add more chain names as needed
+  };
 
-    if (!chainMappings.hasOwnProperty(chainName)) {
-        bot.sendMessage(chatId, 'Invalid chain name.');
-        return;
-    }
+  if (!chainMappings.hasOwnProperty(chainName)) {
+    bot.sendMessage(chatId, 'Invalid chain name.');
+    return;
+  }
 
-    const chainId = chainMappings[chainName];
+  const chainId = chainMappings[chainName];
 
-    // Call the saveToUser function to add the addresses
-    const result = saveToUser(msg.from.id.toString(), chainId, addresses);
+  // Call the saveToUser function to add the addresses
+  const result = saveToUser(msg.from.id.toString(), chainId, addresses);
 
-    if (result.error) {
-        bot.sendMessage(chatId, result.message); // Send the error message to the user
-    } else {
-        bot.sendMessage(chatId, `Added ${addresses.length} addresses to chain ${chainName}`);
-    }
-
-});
-
-// Command: /commandtest
-bot.onText(/\/commandtest/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'The command was: /commandtest');
+  if (result.error) {
+    bot.sendMessage(chatId, result.message); // Send the error message to the user
+  } else {
+    bot.sendMessage(chatId, `Added ${addresses.length} addresses to chain ${chainName}`);
+  }
 });
 
 // General message handler
 bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Received your message');
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Received your message');
 });
+
+// Call the continuouslyGetContracts() function
+continuouslyGetContracts();
